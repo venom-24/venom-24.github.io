f29654e252b1cbd1d5933f34f9397af2b2b5ec9c
Venom24 <93699356+venom-24@users.noreply.github.com>
2026-03-24 22:00:44 -0600

    Update installer_repo.sh

--- installer_repo.sh
+++ installer_repo.sh
@@ -7,9 +7,9 @@ pkg install gnupg wget -y
 
 if [ ! -f "$PREFIX/etc/apt/sources.list.d/venom24.list" ]; then
 
-	echo -e "deb https://github.com/venom-24/Venom24.github.io my-packtools termux " > $PREFIX/etc/apt/sources.list.d/venom24.list
+	echo -e "deb https://github.com/venom-24/venom-24.github.io termux-repo stable main" > $PREFIX/etc/apt/sources.list.d/venom24.list
 
-	wget -qP $PREFIX/etc/apt/trusted.gpg.d https://github.com/venom-24/Venom24.github.io/venom24.gpg
+	wget -qP $PREFIX/etc/apt/trusted.gpg.d https://github.com/venom-24/venom-24.github.io/venom24.gpg
 	apt update
 else 
 	echo "repo already installed"

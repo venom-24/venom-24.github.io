7559d3f79e22771fb8df8e5e57321358ac14f60c
Venom24 <93699356+venom-24@users.noreply.github.com>
2026-03-24 22:53:44 -0600

    Add files via upload

---
+++ installer_repo.sh
@@ -0,0 +1,17 @@
+#! /data/data/com.termux/files/usr/bin/bash
+
+
+pkg install gnupg wget -y
+
+[ ! -d $PREFIX/etc/apt/sources.list.d ] && mkdir $PREFIX/etc/apt/sources.list.d
+
+if [ ! -f "$PREFIX/etc/apt/sources.list.d/venom24.list" ]; then
+
+	echo -e "deb [trusted=yes]   https://venom-24.github.io/termux-repo stable main" > $PREFIX/etc/apt/sources.list.d/venom24.list
+
+	wget -qP $PREFIX/etc/apt/trusted.gpg.d https://venom-24.github.io/venom24.gpg
+	apt update
+else 
+	echo "repo already installed"
+fi 
+
